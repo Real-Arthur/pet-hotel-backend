9e8872f1003af2fb493778250d57792bd69a1d62
Arthur Carson <rthrcrsn@gmail.com>
2020-10-28 17:25:06 -0500

    installed flask-cors thing

--- app/test.py
+++ app/test.py
@@ -1,4 +1,10 @@
 from app import app
+from flask_cors import CORS
+from flask import jsonify
+CORS(app)
+import psycopg2
+conn = psycopg2.connect(user='rthrcrsn', host='localhost',  port=5432, dbname='pet_hotel')
+cur = conn.cursor()
 
 @app.route('/hello')
 def hello():
@@ -6,3 +12,11 @@ def hello():
 @app.route('/spooky')
 def spooky():
     return 'Skeletons'
+
+@app.route('/homepage')
+def customers():
+    cur.execute("SELECT * FROM pets")
+    pets = cur.fetchall()
+    print(pets)
+    for pet in pets:
+        return jsonify(pet)
