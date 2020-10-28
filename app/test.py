4be2e6c5f08c1aa9705d05f771054388eeb36565
CooperDoten <cooper.doten@gmail.com>
2020-10-28 16:26:43 -0500

    added route

--- app/test.py
+++ app/test.py
@@ -1,4 +1,7 @@
 from app import app
+import psycopg2
+conn = psycopg2.connect(user='cooper', host='127.0.0.1',  port=5432, dbname='pet_hotel')
+cur = conn.cursor()
 
 @app.route('/hello')
 def hello():
@@ -6,3 +9,11 @@ def hello():
 @app.route('/spooky')
 def spooky():
     return 'Skeletons'
+
+@app.route('/homepage')
+def customers():
+    cur.execute("SELECT * FROM pets")
+    pets = cur.fetchall()
+    print(pets)
+    for pet in pets:
+        return pet
