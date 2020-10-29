6b3d82f344124d4541d58a8b91cbf4117a945222
Arthur Carson <rthrcrsn@gmail.com>
2020-10-29 09:27:08 -0500

    aaaaahhhhh

--- app/test.py
+++ app/test.py
@@ -1,14 +1,9 @@
 from app import app
-<<<<<<< HEAD
 from flask_cors import CORS
 from flask import jsonify
 CORS(app)
 import psycopg2
 conn = psycopg2.connect(user='rthrcrsn', host='localhost',  port=5432, dbname='pet_hotel')
-=======
-import psycopg2
-conn = psycopg2.connect(user='rthrcrsn', host='localhost', dbname='pet_hotel')
->>>>>>> main
 cur = conn.cursor()
 
 @app.route('/hello')
@@ -23,9 +18,4 @@ def customers():
     cur.execute("SELECT * FROM pets")
     pets = cur.fetchall()
     print(pets)
-<<<<<<< HEAD
     return jsonify(pets)
-=======
-    for pet in pets:
-        return pet
->>>>>>> main
